87f77259bc360b0812a35c529854c2bbcf3ddffb
Valentin Fedoskin <valentin.fedoskin@gmail.com>
2018-09-17 22:50:12 +1000

    add basic inventory service

---
+++ services/inventory/src/main/java/com/github/slamdev/catalog/inventory/Application.java
@@ -0,0 +1,13 @@
+package com.github.slamdev.catalog.inventory;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        run(Application.class, args);
+    }
+}
---
+++ services/inventory/src/main/java/com/github/slamdev/catalog/inventory/integration/CorsConfiguration.java
@@ -0,0 +1,19 @@
+package com.github.slamdev.catalog.inventory.integration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedHeaders("*");
+    }
+}
