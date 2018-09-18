d1f2c58739ca5052666e2490ffa376d443d9ae6b
Valentin Fedoskin <valentin.fedoskin@gmail.com>
2018-09-18 16:56:35 +1000

    add selfhosted k8s gitlab runner deployment

---
+++ ops/gitlab-runner/README.md
@@ -0,0 +1,6 @@
+# Gitlab Runner
+
+Installation of gitlab runner requires [PERSONAL_ACCESS_TOKEN](https://gitlab.com/profile/personal_access_tokens) 
+and [REGISTRATION_TOKEN](https://gitlab.com/slamdev/catalog/settings/ci_cd) environment variables.
+
+Deployment will automatically register a new runner at gitlab.com and remove the old one if it exists.
---
+++ ops/gitlab-runner/deploy/docker/registrator/registrator.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+CI_SERVER_URL="https://gitlab.com"
+
+URL=${CI_SERVER_URL}/api/v4
+
+EXISTING_RUNNERS=`curl --header "PRIVATE-TOKEN: ${PERSONAL_ACCESS_TOKEN}" "${URL}/runners" | \
+    jq ".[] | select(.description == \"${RUNNER_NAME}\") | .id"`
+
+for runner in ${EXISTING_RUNNERS}; do
+    echo "Deleting existing runner: ${runner}"
+    curl --request DELETE --header "PRIVATE-TOKEN: ${PERSONAL_ACCESS_TOKEN}" "${URL}/runners/${runner}"
+done
+
+RUNNER_TOKEN=`curl --request POST "${URL}/runners" \
+    --form "token=${REGISTRATION_TOKEN}" \
+    --form "description=${RUNNER_NAME}" \
+    --form "locked=true" \
+    --form "run_untagged=false" \
+    --form "tag_list=${RUNNER_TAG_LIST}" | \
+    jq -r '.token'`
+
+echo "Runner is registered with token: ${RUNNER_TOKEN}"
+
+cp ${GITLAB_CONFIG_FILE} /etc/gitlab-ranner-patched-config/config.toml
+
+sed -i -e "s/_TOKEN_/${RUNNER_TOKEN}/g" /etc/gitlab-ranner-patched-config/config.toml
