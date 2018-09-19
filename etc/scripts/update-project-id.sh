c56e5dbfe793e205b3b5fd3304b89f9fd3b0ba7a
Valentin Fedoskin <valentin.fedoskin@gmail.com>
2018-09-19 14:34:27 +1000

    add script to update project id

--- etc/scripts/update-project-id.sh
+++ etc/scripts/update-project-id.sh
@@ -5,12 +5,15 @@
 ## mainly to update gcr registry url
 ##
 
-set -euox pipefail
+set -euo pipefail
 
 PROJECT_DIR="../../"
 TF_DIR="../cluster-configuration"
 CURRENT_PROJECT_ID=`(cd ${TF_DIR} && terraform output -module=project project_id)`
-LEGACY_PROJECT_ID=`(cd ${PROJECT_DIR} && cat .gitlab-ci.yml | yq r - -j | jq -re '[.. | .image? | select(type != "null")][0]' | tr "/" "\n" | sed -n 2p)`
 
-grep -rl --exclude-dir="*.git" --exclude-dir="*.idea" --exclude-dir="*.gradle" ${LEGACY_PROJECT_ID} . | \
-    xargs sed -i '' -e s^${LEGACY_PROJECT_ID}^${CURRENT_PROJECT_ID}^g
+cd ${PROJECT_DIR}
+
+LEGACY_PROJECT_ID=`cat .gitlab-ci.yml | yq r - -j | jq -re '[.. | .image? | select(type != "null")][0]' | tr "/" "\n" | sed -n 2p`
+
+grep -rl --exclude-dir="*.git" --exclude-dir="*.idea" --exclude-dir="*.gradle" --exclude-dir="*node_modules" --exclude-dir="*.terraform" --exclude=tfplan ${LEGACY_PROJECT_ID} . | \
+    xargs sed -i '' -e "s/${LEGACY_PROJECT_ID}/${CURRENT_PROJECT_ID}/g"
