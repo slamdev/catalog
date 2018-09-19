2d52f70c20ae4746b069c45a70e692157d48312f
Valentin Fedoskin <valentin.fedoskin@gmail.com>
2018-09-19 14:03:18 +1000

    add gcr login script

--- etc/scripts/gcr-docker-login.sh
+++ etc/scripts/gcr-docker-login.sh
@@ -10,7 +10,7 @@ PROJECT_DIR="../../"
 TF_DIR="../cluster-configuration"
 PROJECT_ID=`(cd ${TF_DIR} && terraform output -module=project project_id)`
 
-asia.gcr.io/catalog-8c920686/catalog-dind
+asia.gcr.io/catalog-8c920686/builder-base
 
 
 grep -rl --exclude-dir="*.git" --exclude-dir="*.idea" --exclude-dir="*.gradle" registry.adnz.co/playground . | xargs sed -i '' -e s^registry.adnz.co/playground^slamdev^g
